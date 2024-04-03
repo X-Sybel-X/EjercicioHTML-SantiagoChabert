1cb2bfd1539c74d552d163a36cb5db2dfde9660c
Sybel <jamuresjamon@gmail.com>
2024-04-03 08:31:06 -0300

    first commit

---
+++ Js del HTML FilippoMartinelli.js
@@ -0,0 +1,8 @@
+function sumar() {
+    var num1 = parseFloat(document.getElementById('numero1').value);
+    var num2 = parseFloat(document.getElementById('numero2').value);
+
+    var resultado = (num1 + num2);
+
+    alert("El resultado es: " + resultado);
+}
